29de24176a4d65e182c2ac84a9bdd2eda692235c
malitharuna <msmalitharuna@gmail.com>
2022-11-30 23:04:31 +0600

    route added

--- src/Routes/PrivateRoute/SellerRoute.js
+++ src/Routes/PrivateRoute/SellerRoute.js
@@ -1,16 +1,18 @@
 import React, { useContext } from 'react';
 import { Navigate, useLocation } from 'react-router-dom';
 import { AuthContext } from '../../contexts/AuthProvider';
+import useSeller from '../../hooks/useSeller';
 
 const PrivateRoute = ({children}) => {
 
     const { user, loading } = useContext(AuthContext);
     const location = useLocation();
+    const [isSeller] = useSeller(user.email);
      
     if(loading){
         return <progress className="progress w-56"></progress>
     }
-    if(user){
+    if(isSeller){
         return children;
     }
     return <Navigate to='/login' state={{from: location}} replace></Navigate>
